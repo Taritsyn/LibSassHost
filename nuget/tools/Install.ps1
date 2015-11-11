243349af3612aedc3a6e0f66a2940fcca36c9ef8
Andrey Taritsyn <taritsyn@gmail.com>
2015-11-11 17:31:47 +0300

    In NuGet package solved the problem with restoring of native assemblies

--- nuget/tools/Install.ps1
+++ nuget/tools/Install.ps1
@@ -2,39 +2,21 @@ param($installPath, $toolsPath, $package, $project)
 
 $assemblyDirectoryName = "LibSassHost.Native"
 $assemblyFileNames = "LibSassHost.Native-32.dll", "LibSassHost.Native-64.dll"
-$projectDirectoryPath = $project.Properties.Item("FullPath").Value
-
-$packageAssemblyDirectoryPath = Join-Path $installPath (Join-Path "content" $assemblyDirectoryName)
-$projectAssemblyDirectoryPath = Join-Path $projectDirectoryPath $assemblyDirectoryName
-
-if (!(Test-Path $projectAssemblyDirectoryPath)) {
-	New-Item -ItemType Directory -Force -Path $projectAssemblyDirectoryPath
-}
-
-foreach ($assemblyFileName in $assemblyFileNames) {
-	$packageAssemblyFilePath = Join-Path $packageAssemblyDirectoryPath $assemblyFileName
-	$projectAssemblyFilePath = Join-Path $projectAssemblyDirectoryPath $assemblyFileName
-
-	if (!(Test-Path $projectAssemblyFilePath)) {
-		Copy-Item $packageAssemblyFilePath $projectAssemblyDirectoryPath
-	}
-}
 
 if ($project.Type -eq "Web Site") {
+	$projectDirectoryPath = $project.Properties.Item("FullPath").Value
 	$binDirectoryPath = Join-Path $projectDirectoryPath "bin"
+	$assemblyDirectoryPath = Join-Path $projectDirectoryPath $assemblyDirectoryName
 
-	if (!(Test-Path $binDirectoryPath)) {
-		New-Item -ItemType Directory -Force -Path $binDirectoryPath
+	if (Test-Path $assemblyDirectoryPath) {
+		if (!(Test-Path $binDirectoryPath)) {
+			New-Item -ItemType Directory -Force -Path $binDirectoryPath
+		}
+
+		Move-Item $assemblyDirectoryPath $binDirectoryPath -Force
 	}
-
-	Move-Item $projectAssemblyDirectoryPath $binDirectoryPath -Force
 }
 else {
 	$assemblyDirectoryItem = $project.ProjectItems.Item($assemblyDirectoryName)
-
-	foreach ($assemblyFileName in $assemblyFileNames) {
-		$assemblyItem = $assemblyDirectoryItem.ProjectItems.Item($assemblyFileName)
-		$assemblyItem.Properties.Item("BuildAction").Value = 0
-		$assemblyItem.Properties.Item("CopyToOutputDirectory").Value = 2
-	}
+	$assemblyDirectoryItem.Delete()
 }
