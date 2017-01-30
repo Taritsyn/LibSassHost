344b1c3ba9175816665e4ba85b4d5e0392fac84f
Andrey Taritsyn <taritsyn@gmail.com>
2017-01-30 20:19:52 +0300

    Fixed a error in NuGet package build script

--- nuget/LibSassHost/build-package.cmd
+++ nuget/LibSassHost/build-package.cmd
@@ -1,6 +1,8 @@
 set project_name=LibSassHost
-set project_source_dir=..\..\src\%project_name%
-set project_bin_dir=%project_source_dir%\bin\Release
+set net4_project_source_dir=..\..\src\%project_name%.Net4
+set net4_project_bin_dir=%net4_project_source_dir%\bin\Release
+set dotnet_project_source_dir=..\..\src\%project_name%
+set dotnet_project_bin_dir=%dotnet_project_source_dir%\bin\Release
 set nuget_package_manager=..\..\.nuget\nuget.exe
 
 call "..\setup.cmd"
@@ -8,16 +10,16 @@ call "..\setup.cmd"
 rmdir lib /Q/S
 del libsass-license.txt /Q/S
 
-%net40_msbuild% "%project_source_dir%\%project_name%.Net40.csproj" /p:Configuration=Release
-xcopy "%project_bin_dir%\%project_name%.dll" lib\net40-client\
+%net40_msbuild% "%net4_project_source_dir%\%project_name%.Net40.csproj" /p:Configuration=Release
+xcopy "%net4_project_bin_dir%\%project_name%.dll" lib\net40-client\
 
-%dotnet_cli% build "%project_source_dir%" --framework net45 --configuration Release --no-dependencies --no-incremental
-xcopy "%project_bin_dir%\net45\%project_name%.dll" lib\net45\
-xcopy "%project_bin_dir%\net45\%project_name%.xml" lib\net45\
+%dotnet_cli% build "%dotnet_project_source_dir%" --framework net45 --configuration Release --no-dependencies --no-incremental
+xcopy "%dotnet_project_bin_dir%\net45\%project_name%.dll" lib\net45\
+xcopy "%dotnet_project_bin_dir%\net45\%project_name%.xml" lib\net45\
 
-%dotnet_cli% build "%project_source_dir%" --framework netstandard1.3 --configuration Release --no-dependencies --no-incremental
-xcopy "%project_bin_dir%\netstandard1.3\%project_name%.dll" lib\netstandard1.3\
-xcopy "%project_bin_dir%\netstandard1.3\%project_name%.xml" lib\netstandard1.3\
+%dotnet_cli% build "%dotnet_project_source_dir%" --framework netstandard1.3 --configuration Release --no-dependencies --no-incremental
+xcopy "%dotnet_project_bin_dir%\netstandard1.3\%project_name%.dll" lib\netstandard1.3\
+xcopy "%dotnet_project_bin_dir%\netstandard1.3\%project_name%.xml" lib\netstandard1.3\
 
 copy "..\..\src\libsass\LICENSE" libsass-license.txt /Y
 
