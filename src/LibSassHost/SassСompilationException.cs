d55eaac97329ff3aca8305726e08bc7124437c3c
Andrey Taritsyn <taritsyn@gmail.com>
2017-01-31 17:06:01 +0300

    `SassСompilationException` class was made serializable

--- src/LibSassHost/SassСompilationException.cs
+++ src/LibSassHost/SassСompilationException.cs
@@ -1,19 +1,51 @@
 ﻿using System;
+#if !NETSTANDARD1_3
+using System.Runtime.Serialization;
+using System.Security.Permissions;
+#endif
 
 namespace LibSassHost
 {
 	/// <summary>
 	/// The exception that is thrown during a Sass compilation
 	/// </summary>
+#if !NETSTANDARD1_3
+	[Serializable]
+#endif
 	public sealed class SassСompilationException : Exception
 	{
+		/// <summary>
+		/// Error status
+		/// </summary>
+		private int _status;
+
+		/// <summary>
+		/// Error text
+		/// </summary>
+		private string _text = string.Empty;
+
+		/// <summary>
+		/// Name of file, where the error occurred
+		/// </summary>
+		private string _file = string.Empty;
+
+		/// <summary>
+		/// Line number
+		/// </summary>
+		private int _lineNumber;
+
+		/// <summary>
+		/// Column number
+		/// </summary>
+		private int _columnNumber;
+
 		/// <summary>
 		/// Gets or sets a error status
 		/// </summary>
 		public int Status
 		{
-			get;
-			set;
+			get { return _status; }
+			set { _status = value; }
 		}
 
 		/// <summary>
@@ -21,8 +53,8 @@ namespace LibSassHost
 		/// </summary>
 		public string Text
 		{
-			get;
-			set;
+			get { return _text; }
+			set { _text = value; }
 		}
 
 		/// <summary>
@@ -30,8 +62,8 @@ namespace LibSassHost
 		/// </summary>
 		public string File
 		{
-			get;
-			set;
+			get { return _file; }
+			set { _file = value; }
 		}
 
 		/// <summary>
@@ -39,8 +71,8 @@ namespace LibSassHost
 		/// </summary>
 		public int LineNumber
 		{
-			get;
-			set;
+			get { return _lineNumber; }
+			set { _lineNumber = value; }
 		}
 
 		/// <summary>
@@ -48,8 +80,8 @@ namespace LibSassHost
 		/// </summary>
 		public int ColumnNumber
 		{
-			get;
-			set;
+			get { return _columnNumber; }
+			set { _columnNumber = value; }
 		}
 
 
@@ -60,12 +92,63 @@ namespace LibSassHost
 		/// <param name="message">The message that describes the error</param>
 		public SassСompilationException(string message)
 			: base(message)
+		{ }
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="SassСompilationException"/> class
+		/// with a specified error message and a reference to the inner exception
+		/// that is the cause of this exception
+		/// </summary>
+		/// <param name="message">The error message that explains the reason for the exception</param>
+		/// <param name="innerException">The exception that is the cause of the current exception</param>
+		public SassСompilationException(string message, Exception innerException)
+			: base(message, innerException)
+		{ }
+#if !NETSTANDARD1_3
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="SassСompilationException"/> class with serialized data
+		/// </summary>
+		/// <param name="info">The object that holds the serialized data</param>
+		/// <param name="context">The contextual information about the source or destination</param>
+		private SassСompilationException(SerializationInfo info, StreamingContext context)
+			: base(info, context)
 		{
-			Status = 0;
-			Text = string.Empty;
-			File = string.Empty;
-			LineNumber = 0;
-			ColumnNumber = 0;
+			if (info != null)
+			{
+				_status = info.GetInt32("Status");
+				_text = info.GetString("Text");
+				_file = info.GetString("File");
+				_lineNumber = info.GetInt32("LineNumber");
+				_columnNumber = info.GetInt32("ColumnNumber");
+			}
 		}
+
+
+		#region Exception overrides
+
+		/// <summary>
+		/// Populates a <see cref="SerializationInfo"/> with the data needed to serialize the target object
+		/// </summary>
+		/// <param name="info">The <see cref="SerializationInfo"/> to populate with data</param>
+		/// <param name="context">The destination (see <see cref="StreamingContext"/>) for this serialization</param>
+		[SecurityPermission(SecurityAction.Demand, SerializationFormatter = true)]
+		public override void GetObjectData(SerializationInfo info, StreamingContext context)
+		{
+			if (info == null)
+			{
+				throw new ArgumentNullException("info");
+			}
+
+			base.GetObjectData(info, context);
+			info.AddValue("Status", _status);
+			info.AddValue("Text", _text);
+			info.AddValue("File", _file);
+			info.AddValue("LineNumber", _lineNumber);
+			info.AddValue("ColumnNumber", _columnNumber);
+		}
+
+		#endregion
+#endif
 	}
 }
