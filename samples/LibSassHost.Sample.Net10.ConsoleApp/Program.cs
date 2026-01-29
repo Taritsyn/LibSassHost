19f36922d98144c20136f22d525ff00c684c7c9f
Taritsyn <taritsyn@gmail.com>
2026-01-29 20:09:49 +0300

    In test and sample projects added support for .NET 10

---
+++ samples/LibSassHost.Sample.Net10.ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using LibSassHost.Sample.Logic;
+
+namespace LibSassHost.Sample.Net10.ConsoleApp
+{
+	class Program : CompilationExampleBase
+	{
+		static void Main(string[] args)
+		{
+			CompileContent();
+			CompileFile();
+		}
+	}
+}
